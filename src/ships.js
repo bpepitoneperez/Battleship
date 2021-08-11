64e46a2a9c748004e8cd8213845088ee2e8ea64c
Bryan Pepitone <brryann21@gmail.com>
2021-08-11 15:07:43 -0400

    Basic ship factory working

--- src/ships.js
+++ src/ships.js
@@ -1,8 +1,35 @@
 const Ship = (title, length) => {
+    let hits = 0;
+    let position = [];
+
+    for (let i = 0; i < length; i++) {
+        position[i] = false;
+    }
+
+    function hit (pos) {
+        position[pos] = true;
+        this.hits++;
+    };
+
+    function isSunk () {
+        for (let i = 0; i < length; i++) {
+            if (position[i] == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     return {
         title,
         length,
+        hit,
+        hits,
+        position,
+        isSunk
     }
 }
+
+
 
 export {Ship};
--- src/ships.test.js
+++ src/ships.test.js
@@ -3,6 +3,25 @@ import {Ship} from './ships.js';
 
 test('creates basic ship', () => {
     const ship = Ship('boat', 3);
-    expect(ship.title).toBe('boat')
-    expect(ship.length).toBe(3)
-})
+    expect(ship.title).toBe('boat');
+    expect(ship.length).toBe(3);
+});
+
+test('can hit ship position', () => {
+    const ship = Ship('battleship', 4);
+    ship.hit(0);
+    expect(ship.position[0]).toBe(true);
+});
+
+test('sink ship check', () => {
+    const ship = Ship('teeny', 2);
+    ship.hit(0);
+    ship.hit(1);
+    expect(ship.isSunk()).toBe(true);
+});
+
+test('sink ship check 2 (not sunk)', () => {
+    const ship = Ship('teeny', 2);
+    ship.hit(0);
+    expect(ship.isSunk()).toBe(false);
+});
